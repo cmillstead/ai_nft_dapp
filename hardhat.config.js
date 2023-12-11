dbb2d1f62d626f4766f549d575d1a06a1a1dd3df
Cevin Millstead <cevinm@gmail.com>
2023-12-11 15:24:30 -0800

    add sepolia add netlify node api urls

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {},
+      sepolia: {
+        url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+        accounts: [privateKey]
+      }
+  }
 };
-
